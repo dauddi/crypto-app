58e8203bbd7f43c494e99b3ccec0a0e028c76f04
Davis Okioma <kuntaytc@gmail.com>
2021-10-12 00:03:28 +0300

    added props to child components

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -3,23 +3,16 @@ import millify from 'millify';
 import { Typography, Row, Col, Statistic } from 'antd';
 import { Link } from 'react-router-dom';
 import {Cryptocurrencies, News} from '../components'
-
 import { useGetCryptosQuery } from '../services/cryptoApi';
-
 
 const { Title } = Typography;
 
 const Homepage = () => {
-	const { data, isFetching } = useGetCryptosQuery();
+	const { data, isFetching } = useGetCryptosQuery(10);
 
-	if (isFetching) return (
-		<>
-			<Title level={2} className="Heading">Global Crypto Stats</Title>
-			<Row>
-				<Col>Loading...</Col>
-			</Row>
-		</>
-	)
+	console.log(data);
+
+	if (isFetching) return "Loading...";
 
 	return (
 		<Fragment>
@@ -27,8 +20,8 @@ const Homepage = () => {
 			<Row>
 				<Col span={12}> <Statistic title="Total Cryptocurrencies" value={data?.data.stats.total} /> </Col>
 				<Col span={12}> <Statistic title="Total Exchanges" value={data?.data.stats.totalExchanges} /> </Col>
-				<Col span={12}> <Statistic title="Total Market Cap" value={millify(data?.data.stats.totalMarketCap)} /> </Col>
-				<Col span={12}> <Statistic title="Total 24h Volume" value={millify(data?.data.stats.total24hVolume)} /> </Col>
+				<Col span={12}> <Statistic title="Total Market Cap" value={millify(data?.data?.stats?.totalMarketCap)} /> </Col>
+				<Col span={12}> <Statistic title="Total 24h Volume" value={millify(data?.data?.stats?.total24hVolume)} /> </Col>
 				<Col span={12}> <Statistic title="Total Markets" value={data?.data.stats.totalMarkets} /> </Col>
 			</Row>
 
@@ -38,14 +31,14 @@ const Homepage = () => {
 					<Link to='/cryptocurrencies'>Show More</Link>
 				</Title>
 			</div>
-			<Cryptocurrencies />
+			<Cryptocurrencies simplified />
 			<div className="home-heading-container">
 				<Title level={2} className="home-title">Latest Crypto News</Title>
 				<Title level={3} className="show-more">
 					<Link to='/news'>Show More</Link>
 				</Title>
 			</div>
-			<News />
+			<News simplified />
 		</Fragment>
 	)
 }

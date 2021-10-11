94b29a1074152e5cd747246fa3f461432b663c82
Davis Okioma <kuntaytc@gmail.com>
2021-10-11 18:30:57 +0300

    added redux Provider

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
-import App from "./App"
+import 'antd/dist/antd.css';
+import App from "./App";
+import { Provider } from "react-redux";
+import store from './app/store';
 
 ReactDOM.render(
 	<BrowserRouter>
-		<App />
+		<Provider store={store}>
+			<App />
+		</Provider>
 	</BrowserRouter>,
-	document.getElementById('root')
-
-);
+	document.getElementById('root'))
